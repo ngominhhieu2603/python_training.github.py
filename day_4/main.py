6993ae008586f041c1787b4e36e31aa2a8e7bda0
ngominhhieu2603 <2603ngominhhieu@gmail.com>
2024-02-19 16:16:25 +0700

    init_commit

--- day_4/main.py
+++ day_4/main.py
@@ -1,19 +1,17 @@
 # Window Basics
 from tkinter import *
 
-# Define Window
-root = Tk()
-root.title("Window Basics!")
-root.iconbitmap("thinking.ico")
-root.geometry("250x700")
-root.resizable(0,0)
-root.config(bg= "blue")
+roof = Tk()
+roof.title("Window Basics!")
+roof.iconbitmap("thinking.ico")
+roof.geometry("250x700")
+roof.resizable(0, 0)
+roof.config(bg= 'blue')
 
-# Second Window
+# second Window
 top = Toplevel()
-top.title("Second Window")
+top.title("Second Windows")
+top.geometry("400x250+400+300")
 top.config(bg= '#123456')
-top.geometry("200x200+500+50")
 
-# Run root Window's mainloop
-root.mainloop()
+roof.mainloop()
